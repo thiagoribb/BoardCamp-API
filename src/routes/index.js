112ccbd30af1ccfc0883be8d20e3513a5c848daf
Caroline <caroline.y.ldm@gmail.com>
2022-03-07 15:30:12 -0300

    rentals done

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import categoryRouter from "./categoryRouter.js";
 import boardRouter from "./boardRouter.js";
 import customerRouter from "./customerRouter.js";
+import rentalRouter from "./rentalRouter.js";
 
 const router = Router();
 
 router.use(categoryRouter);
 router.use(boardRouter);
 router.use(customerRouter);
+router.use(rentalRouter);
 
 export default router;
